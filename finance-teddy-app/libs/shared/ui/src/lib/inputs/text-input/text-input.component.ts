e4ffd5127fc2e806cc2180ee943af3b59cde229e
Júlio César Jordão <julinnn4k@live.com>
2025-03-27 16:03:34 -0300

    feat: add text input to shared library

---
+++ finance-teddy-app/libs/shared/ui/src/lib/inputs/text-input/text-input.component.ts
@@ -0,0 +1,44 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ControlContainer, FormGroupDirective, ReactiveFormsModule, UntypedFormControl } from '@angular/forms';
+
+@Component({
+  selector: 'lib-text-input',
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './text-input.component.html',
+  styleUrl: './text-input.component.scss',
+  viewProviders: [
+    {
+      provide: ControlContainer,
+      useExisting: FormGroupDirective
+    }
+  ]
+})
+export class TextInputComponent {
+  constructor(private formGroup: FormGroupDirective) { }
+
+  @Input() name!: string;
+  @Input() label?: string;
+  @Input() id!: string;
+  @Input() size: 'md' | 'lg' = 'md';
+
+  control!: UntypedFormControl;
+
+  ngOnInit(): void {
+    this.control = this.formGroup.form.get(this.name) as UntypedFormControl;
+
+    if(this.id == undefined) this.id = this.generateUniqueId();
+  }
+
+  generateUniqueId(): string {
+    return `id-${Date.now()}-${Math.floor(Math.random() * 10000)}`;
+  }
+
+  hasError(): boolean {
+    return this.control.invalid && (this.control.dirty || this.control.touched);
+  }
+
+  getErrors(): string {
+    return this.control.errors!['required'] ? 'This field is required' : '';
+  }
+}

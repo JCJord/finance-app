a2ec4787196e17849eb82fcfc88ec7d1ba272199
Júlio César Jordão <julinnn4k@live.com>
2025-03-27 16:01:22 -0300

    chore: set finance-app-teddy as the host application

--- finance-teddy-app/apps/finance-teddy-app/src/app/app.routes.ts
+++ finance-teddy-app/apps/finance-teddy-app/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Route } from '@angular/router';
+import { AppComponent } from './app.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    component: AppComponent,
+    children: [
+      {
+        path: '',
+        loadComponent() {
+          return import('./features/home/home.component').then(m => m.HomeComponent);
+        },
+      },
+    ],
+  },
+];

67db4230a6413768b20c8536c2128ca19f6c3611
Júlio César Jordão <julinnn4k@live.com>
2025-03-28 16:23:37 -0300

    feat: add header and displaying only when its not on home page

--- finance-teddy-app/apps/finance-teddy-app/src/app/app.component.ts
+++ finance-teddy-app/apps/finance-teddy-app/src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { HeaderComponent } from './core/components/header/header.component';
+import { CommonModule } from '@angular/common';
 @Component({
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule, HeaderComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'finance-teddy-app';
+  constructor(private router: Router) {}
+
+  isNotHomePage() {
+    return this.router.url !== '/';
+  }
 }
